a08ac7a8ce25a5e095da9d968c5a13ccf9351606
tb832f <tb832f@MOCDTL77JO5020.ITServices.sbc.com>
2014-11-04 08:39:59 -0600

    added printString method

--- Test/src/test/TestStuff.java
+++ Test/src/test/TestStuff.java
@@ -2,9 +2,16 @@ package test;
 
 public class TestStuff 
 {
+	static String stuff = "stuff";
 	public static void main(String [] args)
 	{
 		System.out.println("hello");
+		printString(stuff);
+	}
+	public static void printString(String s)
+	{
+		System.out.println(s);
+		System.exit(0);
 	}
 
 }
