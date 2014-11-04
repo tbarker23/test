cf281296959a6bd18c591b301fec2e02eca1f167
tbarker23 <barker.tommy@gmail.com>
2014-11-04 07:56:34 -0600

    printing the min and max now

--- Test/src/test/TestStuff.java
+++ Test/src/test/TestStuff.java
@@ -2,8 +2,14 @@ package test;
 
 public class TestStuff 
 {
+	/**@vars*/
+	Integer MAX = Integer.MAX_VALUE; 
+	Integer MIN = Integer.MIN_VALUE);
+	
 	public static void main(String [] args)
 	{
 		System.out.println("hello");
+		System.out.println(MAX);
+		System.out.println(MAX);
 	}
 }
