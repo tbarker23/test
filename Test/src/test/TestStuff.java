140e955e8b7489bfffe8a724ae118b2db3455c32
unknown <tb832f@MOCDTL77JO5020.ITServices.sbc.com>
2014-11-04 08:47:30 -0600

    reset

--- Test/src/test/TestStuff.java
+++ Test/src/test/TestStuff.java
@@ -2,31 +2,5 @@ package test;
 
 public class TestStuff 
 {
-	static String stuff = "stuff";
-	static Integer MAX = Integer.MAX_VALUE;
-	static Integer MIN = Integer.MIN_VALUE;
-	public static void main(String [] args)
-	{
-		System.out.println("hello");
-
-		printString(stuff);
-		System.out.println(MAX);
-		System.out.println(MIN);
-	}
-	public static void printString(String s)
-	{
-		System.out.println(s);
-		System.exit(0);
-	}
-		
-
-	public static void printMax()
-	{
-		System.out.println(MAX);
-	}
-	public static void printMin()
-	{
-		System.out.println(MIN);
-	}
 
 }
