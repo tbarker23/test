4f7336236c4cb207dda55fb4b8b2bc71b0bcc031
unknown <tb832f@MOCDTL77JO5020.ITServices.sbc.com>
2014-11-04 08:56:03 -0600

    added printstring method

--- Test/src/test/TestStuff.java
+++ Test/src/test/TestStuff.java
@@ -2,8 +2,15 @@ package test;
 
 public class TestStuff 
 {
+	static String stuff = "stuff";
+
 	public static void main(String [] args)
 	{
 		System.out.println("hello");
+		printStuff(stuff);
+	}
+	public static void printStuff(String s)
+	{
+		System.out.println(s);
 	}
 }
