8d1ab2e1bea82fb3b1f184921345d2c29146e221
tbarker23 <barker.tommy@gmail.com>
2014-11-04 08:15:45 -0600

    Update TestStuff.java

--- Test/src/test/TestStuff.java
+++ Test/src/test/TestStuff.java
@@ -2,20 +2,5 @@ package test;
 
 public class TestStuff 
 {	
-	Integer MAX = Integer.MAX_VALUE;
-	Integer MIN = Integer.MIN_VALUE;
-	public static void main(String [] args)
-	{
-		System.out.println("hello");
-		System.out.println(MIN);
-		System.out.println(MAX);
-	}
-	public static void printMin()
-	{
-		System.out.println(MIN);
-	}
-	public static void printMax()
-	{
-		System.out.println(MAX);
-	}
+
 }
