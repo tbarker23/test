57d145375db2aeb490e4219e3e3fad7eef3847de
tb832f <tb832f@MOCDTL77JO5020.ITServices.sbc.com>
2014-11-04 08:00:45 -0600

    finished up my print method

--- Test/src/test/TestStuff.java
+++ Test/src/test/TestStuff.java
@@ -2,8 +2,16 @@ package test;
 
 public class TestStuff 
 {
+	static String printStuff = "this is a string";
+
 	public static void main(String [] args)
 	{
 		System.out.println("hello");
+		printString(printStuff);
+	}
+	public static void printString(String s)
+	{
+		System.out.println(s);
+		System.exit(0);
 	}
 }
